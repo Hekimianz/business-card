e43c461afe24776f1574c21eec677c616545ff3c
Aram Hekimian <aramhek121@gmail.com>
2023-03-14 23:03:34 -0600

    Finished about component

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div id="aboutCont">
+      <h2 id="aboutTitle">About</h2>
+      <p id="aboutText">
+        I am a front-end developer from Mexico, currently living in Querétaro,
+        Mexico. I try to keep up with security and best practices, and am always
+        looking for new things to learn.
+      </p>
+    </div>
+  );
+}
