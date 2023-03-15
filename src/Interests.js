5a64afd7ce385f34d04293123d71e585c69b2657
Aram Hekimian <aramhek121@gmail.com>
2023-03-14 23:03:57 -0600

    Finished interests component

---
+++ src/Interests.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Interests() {
+  return (
+    <div id="interestsCont">
+      <h2 id="interestsTitle">Interests</h2>
+      <p id="interestsText">
+        Movie buff. Avid reader. Gamer. Dog lover. Coffee addict. Music nerd.
+      </p>
+    </div>
+  );
+}
